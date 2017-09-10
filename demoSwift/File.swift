bec7470aa65ca436dc7ebbd274a8b25d2ad86d63
Admin <admin@Admins-MacBook-Pro.local>
2017-09-10 20:27:22 +0700

    pdf

---
+++ demoSwift/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  demoSwift
+//
+//  Created by Admin on 9/10/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import Foundation
+
+public struct Common {
+    public let URL: String = "cai gi do o day"
+}
